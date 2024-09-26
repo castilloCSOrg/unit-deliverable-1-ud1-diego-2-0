762d915f12960a86cee2bc04781d5068b0c894ee
Diego Del Real <ddelreal760@gmail.com>
2024-09-26 21:45:54 +0000

    Day 1

--- Main.java
+++ Main.java
@@ -1,14 +1,14 @@
 /********************************************
-*	AUTHORS:	<your names>
+*	AUTHORS: Diego Del Real
 * COLLABORATORS: <name of peer, tutor, instructor, anyone else who helped>
-*	LAST MODIFIED:	<date of last change>
+*	LAST MODIFIED:	09/26/24
 ********************************************/
 
 /********************************************
-*	<TITLE OF PROGRAM>
+*	<Slay The Spire Damage & Block Calculations>
 *********************************************
 *	PROGRAM DESCRIPTION:
-*	<1-2 sentences describing overall program>
+*	Program to calculate how much damage is dealt to a monster when an attack card is played and how much block is gained when playing a block card.
 *********************************************
 *	ALGORITHM:
 *	<Pseudocode here>
@@ -25,15 +25,27 @@ public class Main
   public static void main(String[] args)
   {
     /***** DECLARATION SECTION *****/
+    int attack, defend, playerHealth, playerTotalHP, slimeHealth, slimeAttack;
 
     /***** INITIALIZATION SECTION *****/
+    attack = 6;
+    defend = 5;
+    playerHealth = 25;
+    slimeHealth = 12;
+    slimeAttack = 3;
 
     /***** INTRO SECTION *****/
 
     /***** PROCESSING SECTION *****/
 
     /***** OUTPUT SECTION *****/
-    
+    System.out.println("            Combat Start!\nPlayer Health           Slime Health");
+    System.out.printf("%7d %22d\n", playerHealth, slimeHealth);
   }
   /***** STATIC METHODS *****/
+  public static void attackPlayed()
+  {
+
+  }
+  
 }
