c4924d2bb05e08df99249ecd08e75095e27bda00
Diego Del Real <ddelreal760@gmail.com>
2024-11-25 02:02:19 +0000

    Added double and String variable to meet rubric

--- Main.java
+++ Main.java
@@ -1,11 +1,11 @@
 /********************************************
 *	AUTHORS: Diego Del Real
 * COLLABORATORS: Keith Arcega
-*	LAST MODIFIED:	10/18/24
+*	LAST MODIFIED:	11/24/24
 ********************************************/
 
 /********************************************
-*	<Slay The Spire Damage & Block Calculations>
+*	<Slay The Spire Damage Calculations>
 *********************************************
 *	PROGRAM DESCRIPTION:
 *	Example of a battle between a monster and the player with how much damage is dealt to a monster when an attack card is played and how much block is gained when playing a block card.
@@ -14,7 +14,8 @@
 *	<Pseudocode here>
 *********************************************
 *	STATIC METHODS:
-* <list of static methods and which teammate made each>
+* public static double attackPlayed()
+* public static double slimeAttackPlayed()
 *********************************************/
 
 
@@ -22,10 +23,11 @@ public class Main
 {
   /***** CONSTANT SECTION *****/
   
+    static String playerName = "Ironclad";
     static int attack = 6;
-    static int playerHealth = 25;
-    static int slimeHealth = 12;
-    static int slimeAttack = 3;
+    static double playerHealth = 25;
+    static double slimeHealth = 12;
+    static double slimeAttack = 3.5;
     static int turnCount = 0;
   public static void main(String[] args)
   {
@@ -42,27 +44,27 @@ public class Main
     /***** OUTPUT SECTION *****/
     while(playerHealth > 0 && slimeHealth > 0)
     {
-      System.out.println("            Combat Start!\nPlayer Health           Slime Health");
-      System.out.printf("%7d %22d\n", playerHealth, slimeHealth);
+      System.out.println("            Combat Start!\n" + playerName + " Health           Slime Health");
+      System.out.printf("%7.1f %22.1f\n", playerHealth, slimeHealth);
       System.out.println("              Turn " + ++turnCount + "!");
-      System.out.println("Player used Attack!");
+      System.out.println(playerName + " used Attack!");
       System.out.println("Slime's health is now " + Main.attackPlayed() + "!");
       System.out.println("Slime used Attack!");
-      System.out.println("Player's Health is now " + Main.slimeAttackPlayed() + "!");
+      System.out.println(playerName + "'s Health is now " + Main.slimeAttackPlayed() + "!");
     }
     if(playerHealth > 0)
-      System.out.println("\n            Player wins!");
+      System.out.println("\n            " + playerName + " wins!");
     else
       System.out.println("\n            Slime wins!");
  
   }
   /***** STATIC METHODS *****/
-  public static int attackPlayed()
+  public static double attackPlayed()
   {
     slimeHealth -= attack;
     return slimeHealth;
   }
-  public static int slimeAttackPlayed()
+  public static double slimeAttackPlayed()
   {
     playerHealth -= slimeAttack;
     return playerHealth;
